17e68aacb9cc99e6e54f9ad09adcf0c09e333ebe
ye0jun <ye0jun@naver.com>
2016-04-25 02:13:06 +0900

    유저db만들고 create 및 login 액션 만들어줌

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+require 'bcrypt'
+
+class User < ActiveRecord::Base
+  include BCrypt
+
+  def password
+    @passwor ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+end
